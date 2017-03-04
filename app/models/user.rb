746273628d83962a4f871c155447d897924f0572
Michelle Luby <theatrejawa@yahoo.com>
2017-03-04 23:16:52 +0000

    Added course enrollment for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,32 @@
 class User < ApplicationRecord
   has_many :courses
-  has_many :sections
-  has_many :lessons
-  has_many :pictures
+  # has_many :sections
+  # has_many :lessons
+  # has_many :pictures
+  has_many :enrollments
+  has_many :enrolled_courses, through: :enrollments, source: :course
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def enrolled_in?(course)
+  #    enrolled_courses = []
+  #    enrollments.each do |enrollment|
+  #    enrolled_courses << enrollment.course
+  #   # below is equivalent of above
+
+  #   # enrolled_courses = enrollments.collect do |enrollment|
+  #   #   enrollment.course
+  #   # end
+  #   #below is equivalent of both of the above
+
+    # enrolled_courses = enrollments.collect(&:course)
+  #   # above is an N+1 query. After changing connections to has_many enrolled courses/through, the below is more acceptable
+    
+
+    return enrolled_courses.include?(course)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170301014302) do
+ActiveRecord::Schema.define(version: 20170304214424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 20170301014302) do
     t.text     "caption"
     t.string   "picture"
     t.index ["user_id"], name: "index_courses_on_user_id", using: :btree
+  end
+
+  create_table "enrollments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_enrollments_on_course_id", using: :btree
+    t.index ["user_id", "course_id"], name: "index_enrollments_on_user_id_and_course_id", using: :btree
   end
 
   create_table "images", force: :cascade do |t|
