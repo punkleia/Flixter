e430320fb020db14e25197321d5dbf48477355ec
Michelle Luby <theatrejawa@yahoo.com>
2017-03-07 00:08:24 +0000

    fixed application.yml issue

--- Gemfile
+++ Gemfile
@@ -78,3 +78,5 @@ gem "mini_magick"
 gem "carrierwave-aws"
 
 gem "stripe"
+
+gem "ranked-model"
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,5 +2,7 @@ class Lesson < ApplicationRecord
   belongs_to :section
   has_many :videos 
   
-  mount_uploader :video, VideoUploader
+  include RankedModel
+  ranks :row_order, with_same: :section_id
+  #mount_uploader :video, VideoUploader
 end
